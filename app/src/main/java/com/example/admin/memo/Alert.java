b599f3e82e533a2b766e9425cd785d5c5920d668
Nhr2016011334 <794847434@qq.com>
2018-12-24 15:55:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/admin/memo/Alert.java
@@ -0,0 +1,33 @@
+package com.example.admin.memo;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app. AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Alert extends  AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        String msg = getString(R.string.alarmtext) + getIntent().getExtras().getString(getString(R.string.title_msg));
+        builder.setMessage(msg).setCancelable(
+                false).setPositiveButton(getString(R.string.ok),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        Alert.this.finish();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+}
